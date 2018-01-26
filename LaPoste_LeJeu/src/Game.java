187f83745a1ed51fc8b2b7fc464c3b5fc56d2626
GlobalFistingLeague <bastien@Wooden-Arch.localdomain>
2018-01-26 23:15:39 +0100

    ctrl+maj+f

--- LaPoste_LeJeu/src/Game.java
+++ LaPoste_LeJeu/src/Game.java
@@ -3,39 +3,43 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class Game extends StateBasedGame {
-	public Game(String name) {
+public class Game extends StateBasedGame
+{
+	public Game(String name)
+	{
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void initStatesList(GameContainer gc) throws SlickException {
+	public void initStatesList(GameContainer gc) throws SlickException
+	{
 		// TODO Auto-generated method stub
 		//
 
 		addState(new MenuStart());
 		addState(new PhaseVoiture());
-		
-		//gc.setTargetFrameRate(120);
 
-		
+		// gc.setTargetFrameRate(120);
+
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 		AppGameContainer app;
-		try {
-		app = new AppGameContainer(new Game("jeu"));
-		app.setDisplayMode(1600, 900, false);
-		app.setShowFPS(true);
-		app.start();
-		app.setVSync(false);
+		try
+		{
+			app = new AppGameContainer(new Game("jeu"));
+			app.setDisplayMode(1600, 900, false);
+			app.setShowFPS(true);
+			app.start();
+			app.setVSync(false);
 
-		} catch(SlickException e){
-		e.printStackTrace();
+		} catch (SlickException e)
+		{
+			e.printStackTrace();
 		}
 	}
-	
-	
+
 }
--- LaPoste_LeJeu/src/MenuStart.java
+++ LaPoste_LeJeu/src/MenuStart.java
@@ -4,28 +4,33 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class MenuStart extends BasicGameState {
+public class MenuStart extends BasicGameState
+{
 
 	@Override
-	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException
+	{
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException
+	{
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException
+	{
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public int getID() {
+	public int getID()
+	{
 		// TODO Auto-generated method stub
 		return 0;
 	}
