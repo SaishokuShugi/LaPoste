10c0f75b99fb20526d75aca6a3a6fc76b0010ac8
stduhpf <stephduh@live.fr>
2018-01-26 21:31:03 +0100

    change

--- LaPoste_LeJeu/src/main.java
+++ LaPoste_LeJeu/src/main.java
@@ -2,14 +2,10 @@
 public class main
 {
 
-	public main()
-	{
-		// TODO Stub du constructeur généré automatiquement
-	}
-
 	public static void main(String[] args)
 	{
 		// TODO Stub de la méthode généré automatiquement
+		System.out.println("bite");
 
 	}
 
