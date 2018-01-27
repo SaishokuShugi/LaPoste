1318e7cb7e326703b25db5dc5292963a1e48f4df
GlobalFistingLeague <bastien@Wooden-Arch.localdomain>
2018-01-27 03:15:26 +0100

    +

--- LaPoste_LeJeu/src/MenuStart.java
+++ LaPoste_LeJeu/src/MenuStart.java
@@ -46,7 +46,7 @@ public class MenuStart extends BasicGameState
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException
 	{
-		startBackground.draw(0, 0);
+		startBackground.draw(75, 0);
 		g.setColor(new Color(255, 255, 255, 100));
 		g.fillRect(0, 0, Game.app.getWidth(), Game.app.getHeight());
 		slicker.drawString(700, 400, "Start", Color.lightGray);
