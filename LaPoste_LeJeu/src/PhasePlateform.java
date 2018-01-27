26f3baf014495321aa0f74dbf20a3f108a1d55f4
stduhpf <stephduh@live.fr>
2018-01-27 16:35:22 +0100

    xdcfvbn,;:

--- LaPoste_LeJeu/src/PhasePlateform.java
+++ LaPoste_LeJeu/src/PhasePlateform.java
@@ -2,12 +2,16 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class PhasePlateform extends BasicGameState
-{
-	protected Image bg;
+{Perso
+	protected Image bg,imgPerso;
+	protected SpriteSheet perso;
+	protected float persoX,persoY;
+	final float g = 9.81f;
 
 	public PhasePlateform()
 	{
@@ -16,19 +20,22 @@ public class PhasePlateform extends BasicGameState
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException
 	{
-		bg = new Image("back.jpg");
+		bg = new Image("res/back.jpg");
+		perso = new SpriteSheet("res/Postier.png", 30, 54);
+		imgPerso = perso.getSprite(0, 0);
+		
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException
 	{
-
+		
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException
 	{
-
+		
 	}
 
 	@Override
