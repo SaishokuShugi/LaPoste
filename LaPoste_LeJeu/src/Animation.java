f3af89fa0fbddfc1e4b9d3ff89b689a234c04762
stduhpf <stephduh@live.fr>
2018-01-27 05:10:15 +0100

    time based animation

---
+++ LaPoste_LeJeu/src/Animation.java
@@ -0,0 +1,22 @@
+
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SpriteSheet;
+
+public class Animation
+{
+	private SpriteSheet ss;
+	private int f;
+	
+	public Animation(SpriteSheet ss, int f)
+	{
+		this.ss = ss;
+		this.f = f;
+	}
+	public void draw(float x, float y,Graphics g,float t) {
+		int i = (int)(t/f)%ss.getHorizontalCount();
+		int j = ((int)(t/f)/ss.getHorizontalCount())%ss.getVerticalCount();
+		g.drawImage(ss.getSprite(i, j),x, y);
+	}
+
+}
