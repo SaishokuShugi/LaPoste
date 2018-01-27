3406784be834324136ede256dbe11bd9dff885cf
GlobalFistingLeague <bastien@Wooden-Arch.localdomain>
2018-01-27 16:41:33 +0100

    0

--- LaPoste_LeJeu/src/PhaseVoiture.java
+++ LaPoste_LeJeu/src/PhaseVoiture.java
@@ -42,24 +42,24 @@ public class PhaseVoiture extends BasicGameState
 	public void initBords()
 	{
 		for (int i = 0; i < 5; i++)
-			{
-				bordsSold[i] = Game.random.nextInt(6);
-				bordsSolg[i] = Game.random.nextInt(6);
+		{
+			bordsSold[i] = Game.random.nextInt(6);
+			bordsSolg[i] = Game.random.nextInt(6);
 
-				bordsToitd[i] = Game.random.nextInt(6);
-				bordsToitg[i] = Game.random.nextInt(6);
+			bordsToitd[i] = Game.random.nextInt(6);
+			bordsToitg[i] = Game.random.nextInt(6);
 
-				centreTrot[i] = Game.random.nextInt(4);
-			}
+			centreTrot[i] = Game.random.nextInt(4);
+		}
 	}
 
 	public void updateTrot()
 	{
 		for (int j = 4; j >= 0; j--)
-			{
-				bordsSold[j + 1] = bordsSold[j];
-				bordsSolg[j + 1] = bordsSolg[j];
-			}
+		{
+			bordsSold[j + 1] = bordsSold[j];
+			bordsSolg[j + 1] = bordsSolg[j];
+		}
 		bordsSold[0] = Game.random.nextInt(6);
 		bordsSolg[0] = Game.random.nextInt(6);
 	}
@@ -67,19 +67,19 @@ public class PhaseVoiture extends BasicGameState
 	public void updateCentre()
 	{
 		for (int j = 4; j >= 0; j--)
-			{
-				centreTrot[j + 1] = centreTrot[j];
-			}
+		{
+			centreTrot[j + 1] = centreTrot[j];
+		}
 		centreTrot[0] = Game.random.nextInt(4);
 	}
 
 	public void updateToit()
 	{
 		for (int j = 4; j >= 0; j--)
-			{
-				bordsToitd[j + 1] = bordsToitd[j];
-				bordsToitg[j + 1] = bordsToitg[j];
-			}
+		{
+			bordsToitd[j + 1] = bordsToitd[j];
+			bordsToitg[j + 1] = bordsToitg[j];
+		}
 		bordsToitd[0] = Game.random.nextInt(6);
 		bordsToitg[0] = Game.random.nextInt(6);
 	}
@@ -95,8 +95,7 @@ public class PhaseVoiture extends BasicGameState
 		Trottoirs = new SpriteSheet("res/SpriteRoute.png", 256, 256);
 		Toits = new SpriteSheet("res/SpriteToit.png", 256, 256);
 		CentreSprite = new SpriteSheet("res/SpriteCenter.png", 128, 256);
-		VoitureHitbox = new Rectangle(VoitureX + VoitureHero.getWidth() / 4f, VoitureY + VoitureHero.getWidth() / 6f,
-				VoitureHero.getWidth() / 2f, VoitureHero.getHeight() * 2f / 3f);
+		VoitureHitbox = new Rectangle(VoitureX + VoitureHero.getWidth() / 4f, VoitureY + VoitureHero.getWidth() / 6f, VoitureHero.getWidth() / 2f, VoitureHero.getHeight() * 2f / 3f);
 		initBords();
 		perte = new Animation(new SpriteSheet("res/PerteCourrier.png", 128, 128), 50);
 	}
@@ -106,59 +105,59 @@ public class PhaseVoiture extends BasicGameState
 	{
 
 		for (int i = 0; i < (int) (Game.app.getHeight() / 128f) + 1; i++)
-			{
-				g.drawImage(Road, Game.app.getWidth() * .5f + 64f, (float) ((i - 1) * 128. + posroad % 128.));
-			}
+		{
+			g.drawImage(Road, Game.app.getWidth() * .5f + 64f, (float) ((i - 1) * 128. + posroad % 128.));
+		}
 		Road.rotate(180);
 		for (int i = 0; i < (int) (Game.app.getHeight() / 128f) + 1; i++)
-			{
-				g.drawImage(Road, Game.app.getWidth() * .5f - 320f, (float) ((i - 1) * 128. + posroad % 128.));
-			}
+		{
+			g.drawImage(Road, Game.app.getWidth() * .5f - 320f, (float) ((i - 1) * 128. + posroad % 128.));
+		}
 		Road.rotate(180);
 
 		for (int i = 0; i < (int) (Game.app.getHeight() / 256f) + 2; i++)
-			{
-				float dep = (float) ((i - 1) * 256. + posroad % 256.);
-				Image trott = CentreSprite.getSprite(centreTrot[i] % 2, centreTrot[i] / 2);
-				g.drawImage(trott, Game.app.getWidth() * .5f - 64f, dep);
-			}
+		{
+			float dep = (float) ((i - 1) * 256. + posroad % 256.);
+			Image trott = CentreSprite.getSprite(centreTrot[i] % 2, centreTrot[i] / 2);
+			g.drawImage(trott, Game.app.getWidth() * .5f - 64f, dep);
+		}
 
 		for (int i = 0; i < (int) (Game.app.getHeight() / 256f) + 2; i++)
-			{
-				float dep = (float) ((i - 1) * 256. + posroad % 256.);
-				Image trott = Trottoirs.getSprite(bordsSold[i] % 2, bordsSold[i] / 2);
-				g.drawImage(trott, Game.app.getWidth() * .5f + 320f, dep);
-				trott = Trottoirs.getSprite(bordsSolg[i] % 2, bordsSolg[i] / 2);
-				trott.rotate(180);
-				g.drawImage(trott, Game.app.getWidth() * .5f - 576f, dep);
-			}
+		{
+			float dep = (float) ((i - 1) * 256. + posroad % 256.);
+			Image trott = Trottoirs.getSprite(bordsSold[i] % 2, bordsSold[i] / 2);
+			g.drawImage(trott, Game.app.getWidth() * .5f + 320f, dep);
+			trott = Trottoirs.getSprite(bordsSolg[i] % 2, bordsSolg[i] / 2);
+			trott.rotate(180);
+			g.drawImage(trott, Game.app.getWidth() * .5f - 576f, dep);
+		}
 
 		g.drawImage(VoitureHero, VoitureX, VoitureY);
 
 		Voitures.removeIf((Voiture Voitures) -> (Voitures.render(g)));
 		for (Lampadaire l : lampes)
-			{
-				l.render(g);
-			}
+		{
+			l.render(g);
+		}
 		for (int i = 0; i < (int) (Game.app.getHeight() / 256f) + 2; i++)
-			{
-				float dep = (float) ((i - 1) * 256. + postoit % 256.);
-				Image trott = Toits.getSprite(bordsToitd[i] % 2, bordsToitd[i] / 2);
-				g.drawImage(trott, Game.app.getWidth() * .5f + 320f, dep);
-				trott = Toits.getSprite(bordsToitg[i] % 2, bordsToitg[i] / 2);
-				trott.rotate(180);
-				g.drawImage(trott, Game.app.getWidth() * .5f - 576f, dep);
-			}
+		{
+			float dep = (float) ((i - 1) * 256. + postoit % 256.);
+			Image trott = Toits.getSprite(bordsToitd[i] % 2, bordsToitd[i] / 2);
+			g.drawImage(trott, Game.app.getWidth() * .5f + 320f, dep);
+			trott = Toits.getSprite(bordsToitg[i] % 2, bordsToitg[i] / 2);
+			trott.rotate(180);
+			g.drawImage(trott, Game.app.getWidth() * .5f - 576f, dep);
+		}
 
 		for (PaperParticle pp : lastPerte)
-			{
-				if ((time - pp.t0) < 10 * perte.f)
-					perte.draw(pp.x, pp.y, g, time - pp.t0);
-			}
+		{
+			if ((time - pp.t0) < 10 * perte.f)
+				perte.draw(pp.x, pp.y, g, time - pp.t0);
+		}
 		lastPerte.removeIf((PaperParticle lastPerte) -> (time - lastPerte.t0 > 10 * perte.f));
 		Progression.draw(300, 0);
-		minicar.draw(300+posroad/100, 0);
-		
+		minicar.draw(300 + posroad / 100, 0);
+
 	}
 
 	@Override
@@ -166,17 +165,16 @@ public class PhaseVoiture extends BasicGameState
 	{
 
 		if ((Game.input.isKeyDown(Input.KEY_ESCAPE) || Game.input.isKeyDown(Input.KEY_P)) & timeP > 250 || Game.nlettres <= 0)
-			{
-				timeP = 0;
-				sbg.enterState(4);
-			}
+		{
+			timeP = 0;
+			sbg.enterState(4);
+		}
 		time += delta;
 		timeP += delta;
 		VoitureX += vitesseX * delta;
 		if ((VoitureX + 64 < 480 | VoitureX + 64 > Game.app.getWidth() - 480) && time - timerp > 100)
 			iscarhit = true;
-		VoitureX = VoitureX + 64 < 400 ? 400 - 64
-				: VoitureX + 64 > Game.app.getWidth() - 400 ? Game.app.getWidth() - 400 - 64 : VoitureX;
+		VoitureX = VoitureX + 64 < 400 ? 400 - 64 : VoitureX + 64 > Game.app.getWidth() - 400 ? Game.app.getWidth() - 400 - 64 : VoitureX;
 		vitesseX *= Math.pow(.992, delta);
 		if (Game.input.isKeyDown(Input.KEY_LEFT))
 			vitesseX -= delta * .001 * speed;
@@ -195,34 +193,34 @@ public class PhaseVoiture extends BasicGameState
 		Voitures.removeIf((Voiture Voitures) -> (Voitures.update(delta))); // cherche pas c'est magique
 		lampes.removeIf((Lampadaire lampes) -> (lampes.update(delta)));
 		if ((int) ((speed - Voiture.v0) * time / 6000) > (int) ((speed - Voiture.v0) * (time - delta) / 6000))
-			{
-				if (Game.random.nextInt(3) <= 1)
-					Voitures.add(new VoitureUp());
-			}
+		{
+			if (Game.random.nextInt(3) <= 1)
+				Voitures.add(new VoitureUp());
+		}
 		if ((int) ((speed + Voiture.v0) * time / 6000) > (int) ((speed + Voiture.v0) * (time - delta) / 6000))
-			{
-				if (Game.random.nextInt(3) <= 1)
-					Voitures.add(new VoitureDown());
-			}
+		{
+			if (Game.random.nextInt(3) <= 1)
+				Voitures.add(new VoitureDown());
+		}
 		if ((int) (speed * time / 6000) > (int) (speed * (time - delta) / 6000))
-			{
-				if (Game.random.nextInt(3) <= 1)
-					lampes.add(new Lampadaire());
-			}
+		{
+			if (Game.random.nextInt(3) <= 1)
+				lampes.add(new Lampadaire());
+		}
 		if ((int) (posroad / 256f) != (int) ((posroad += .1 * speed * delta) / 256f))
-			{
-				updateTrot();
-				updateCentre();
-			}
+		{
+			updateTrot();
+			updateCentre();
+		}
 		if ((int) (postoit / 256f) != (int) ((postoit += .103 * speed * delta) / 256f))
 			updateToit();
 
 		if (iscarhit)
-			{
-				Game.nlettres--;
-				lastPerte.add(new PaperParticle());
-				timerp = time;
-			}
+		{
+			Game.nlettres--;
+			lastPerte.add(new PaperParticle());
+			timerp = time;
+		}
 		iscarhit = false;
 
 	}
