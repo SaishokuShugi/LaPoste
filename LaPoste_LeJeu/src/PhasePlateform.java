6baa28056299b7776b13aa42ef6ed397cdc07083
stduhpf <stephduh@live.fr>
2018-01-27 20:21:35 +0100

    pshj

--- LaPoste_LeJeu/src/PhasePlateform.java
+++ LaPoste_LeJeu/src/PhasePlateform.java
@@ -21,7 +21,7 @@ public class PhasePlateform extends BasicGameState
 	private static ArrayList<Rectangle>	Platforms	= new ArrayList<Rectangle>();
 	private Vector2f					Vperso;
 	private boolean						facingLeft	= false, onGround = false;
-	private boolean[]					ispassage	= new boolean[16];
+	private boolean[]					isvoid		= new boolean[16];
 	private Image[]						bgtex		= new Image[16];
 
 	private SpriteSheet					bgvoid, bgmaison, bgscales;
@@ -32,31 +32,46 @@ public class PhasePlateform extends BasicGameState
 
 	private void initmap()
 	{
+		bgtex[0] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[1] = bgvoid.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[2] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[3] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[4] = bgvoid.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[5] = bgscales.getSprite(1, 1);
+		bgtex[6] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[7] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[8] = bgscales.getSprite(0, 1);
+		bgtex[9] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[10] = bgvoid.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[11] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[12] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[13] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+		bgtex[14] = bgscales.getSprite(1, 0);
+		bgtex[15] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
+
+		isvoid[0] = false;
+		isvoid[1] = true;
+		isvoid[2] = false;
+		isvoid[3] = false;
+		isvoid[4] = true;
+		isvoid[5] = false;
+		isvoid[6] = false;
+		isvoid[7] = false;
+		isvoid[8] = false;
+		isvoid[9] = false;
+		isvoid[10] = true;
+		isvoid[11] = false;
+		isvoid[12] = false;
+		isvoid[13] = false;
+		isvoid[14] = false;
+		isvoid[15] = false;
+
 		for (int i = 0; i < 4; i++)
 			for (int j = 0; j < 4; j++)
 				{
-					int n = Game.random.nextInt(4);
-					ispassage[j + 4 * i] = j == n;
-				}
-		for (int i = 0; i < 4; i++)
-			for (int j = 0; j < 4; j++)
-				{
-					if (ispassage[j + 4 * i])
-						{
-							bgtex[j + 4 * i] = bgscales.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
-						} else
-						{
-							if (i < 3)
-								{
-									if (ispassage[j + 4 * (i + 1)])
-										{
-											bgtex[j + 4 * i] = bgvoid.getSprite(Game.random.nextInt(2),
-													Game.random.nextInt(2));
-											return;
-										}
-								}
-							bgtex[j + 4 * i] = bgmaison.getSprite(Game.random.nextInt(2), Game.random.nextInt(2));
-						}
+					if (!isvoid[j + i * 4])
+						Platforms.add(new Rectangle(i * Game.app.getWidth() * .25f, (j+1) * Game.app.getHeight() * .25f,
+								Game.app.getWidth() * .25f, 1));
 				}
 
 	}
@@ -67,7 +82,7 @@ public class PhasePlateform extends BasicGameState
 		bg = new Image("res/back.jpg");
 		bgvoid = new SpriteSheet("res/Rien.png", 400, 225);
 		bgmaison = new SpriteSheet("res/Maison.png", 400, 225);
-		bgscales = new SpriteSheet("res/Plateforme.png", 400, 255);
+		bgscales = new SpriteSheet("res/Plateforme.png", 400, 225);
 
 		perso = new SpriteSheet("res/Postier.png", 42, 74);
 		imgPerso = perso.getSprite(8, 1);
@@ -75,12 +90,18 @@ public class PhasePlateform extends BasicGameState
 		Pperso = new Vector2f(100, 20);
 		hitboxPerso = new Rectangle(Pperso.x, Pperso.y, 42, 74);
 		Vperso = new Vector2f(0, 0);
+		initmap();
+
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException
 	{
-		g.drawImage(bg, 0, 0);
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+				{
+					g.drawImage(bgtex[j + 4 * i], 400 * j, 225 * i);
+				}
 		g.drawImage(imgPerso, Pperso.x, Pperso.y);
 	}
 
