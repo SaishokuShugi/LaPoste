ae99a1cf5fac73a453500fdee71a47ca289985ca
stduhpf <stephduh@live.fr>
2018-01-27 02:15:54 +0100

    Voitures ennemies

--- LaPoste_LeJeu/src/VoitureDown.java
+++ LaPoste_LeJeu/src/VoitureDown.java
@@ -1,25 +1,37 @@
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 
-public class VoitureDown extends Voiture {
+public class VoitureDown extends Voiture
+{
 
-	public VoitureDown() throws SlickException {
-		y = 0f;
-		x = Game.app.getWidth() * .5f - 64f - 200f;
-		image = new Image("res/Car2.png");
+	public VoitureDown() throws SlickException
+	{
+		y = -128f;
+		x = Game.app.getWidth() * .5f - 64f - 256f;
+		voituresImg = new SpriteSheet("res/SpriteSheetCar.png",128,128);
+		image = voituresImg.getSprite(Game.random.nextInt(3), Game.random.nextInt(3));
 		image.rotate(180f);
 		speed = .5f;
+		if (Game.random.nextBoolean())
+			{
+				speed = .55f;
+				x += 128f;
+			}
 	}
 
 	@Override
-	public void render(Graphics g) {
+	public void render(Graphics g)
+	{
 		g.drawImage(image, x, y);
 	}
 
 	@Override
-	public void update(int delta) {
+	public boolean update(int delta)
+	{
 		y += delta * speed;
+		return y > Game.app.getHeight();
 	}
 
 }
