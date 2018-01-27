2a522bc26799617d4f07d732edb4f8a32e2cf0c7
GlobalFistingLeague <bastien@Wooden-Arch.localdomain>
2018-01-27 23:17:13 +0100

    +

---
+++ LaPoste_LeJeu/src/GameOver.java
@@ -0,0 +1,38 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class GameOver extends BasicGameState
+{
+
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException
+	{
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException
+	{
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException
+	{
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	@Override
+	public int getID()
+	{
+		// TODO Stub de la méthode généré automatiquement
+		return 0;
+	}
+
+}
