e131ed4973b104181c70abc56e2efa65ff11116f
GlobalFistingLeague <bastien@Wooden-Arch.localdomain>
2018-01-28 14:50:11 +0100

    +

--- LaPoste_LeJeu/src/poste/Game.java
+++ LaPoste_LeJeu/src/poste/Game.java
@@ -93,7 +93,7 @@ public class Game extends StateBasedGame
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-
+		System.setProperty("java.library.path", "SuperMailBoy_lib");
 		try
 			{
 				app = new AppGameContainer(new Game("jeu"));
